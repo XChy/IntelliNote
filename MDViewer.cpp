7708354c69b753a0be9969f6bb0235fe82d39c2f
XChy <xxs_chy@outlook.com>
2023-05-05 00:05:09 +0800

    Fix the bug of formula rendering add update markdown editor

--- MDViewer.cpp
+++ MDViewer.cpp
@@ -1,6 +1,7 @@
 #include "MDViewer.h"
 #include <QDesktopServices>
 #include <QWebChannel>
+#include <QWebEngineSettings>
 
 void Document::setText(const QString &text)
 {
@@ -21,12 +22,14 @@ bool PreviewPage::acceptNavigationRequest(const QUrl &url,
     return false;
 }
 
-#include <QFile>
-#include <iostream>
 MDViewer::MDViewer(QWidget* parent):QWebEngineView(parent),m_page(new PreviewPage(parent))
 {
     setPage(m_page);
 
+    settings()->setAttribute(QWebEngineSettings::AutoLoadIconsForPage,false);
+    settings()->setAttribute(QWebEngineSettings::WebGLEnabled,false);
+    settings()->setAttribute(QWebEngineSettings::Accelerated2dCanvasEnabled,true);
+    settings()->setAttribute(QWebEngineSettings::DnsPrefetchEnabled,false);
     QWebChannel *channel = new QWebChannel(parent);
     channel->registerObject(QStringLiteral("content"), &m_content);
     m_page->setWebChannel(channel);
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,14 +3,21 @@
 #include <qtextdocument.h>
 #include <qtextedit.h>
 #include <qtextformat.h>
+#include <qmarkdowntextedit/markdownhighlighter.h>
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->splitter->setStretchFactor(0,1);
+    ui->splitter->setStretchFactor(1,2);
+    ui->splitter->setStretchFactor(2,4);
 
-    connect(ui->textEdit, &QTextEdit::textChanged, [=]() {
+    auto doc = ui->textEdit->document();
+    auto* highlighter = new MarkdownHighlighter(doc);
+
+    connect(ui->textEdit, &QPlainTextEdit::textChanged, [=]() {
         ui->viewer->setText(ui->textEdit->toPlainText());
     });
 }
