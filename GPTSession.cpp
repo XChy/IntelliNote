726fc99d9e19b4ca2a6d4032ab58a64e11ea6898
Scotchingice <1439921564@qq.com>
2023-05-06 01:36:06 +0800

    gpt_basics_test of prompt_sending

--- GPTSession.cpp
+++ GPTSession.cpp
@@ -3,6 +3,9 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QUrlQuery>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
 
 GPTSession::GPTSession(QObject *parent)
     : QObject{parent}, APIKey(default_api_key)
@@ -15,25 +18,52 @@ QString GPTSession::getAPIKey() { return APIKey; }
 
 void GPTSession::addPrompt(const QString &prompt)
 {
-    QNetworkAccessManager *manager = new QNetworkAccessManager();
-    QUrl url("https://api.openai.com/v1/engines/davinci-codex/completions");
-    QUrlQuery params;
-    params.addQueryItem("prompt", prompt);
-    params.addQueryItem("max_tokens", QString::number(maxTokens));
-    url.setQuery(params);
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    QUrl url("https://api.openai.com/v1/completions");
+//    QUrlQuery params;
+//    params.addQueryItem("prompt", prompt);
+//    params.addQueryItem("max_tokens", QString::number(maxTokens));
+//    url.setQuery(params);
     QNetworkRequest request(url);
     // the QNetworkRequest class holds a request to be sent with
     // QNetworkAccessManager.
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
     request.setRawHeader("Authorization", ("Bearer " + APIKey).toUtf8());
     // token means APIKey
-    QNetworkReply *reply = manager->post(request, QByteArray());
+    QNetworkReply *reply = manager->post(request, QString("{\n"
+                                                  "\"model\" : \"text-davinci-003\","
+                                                  " \"prompt\": \"%1\" "
+                                                  "\n}").arg(prompt).toUtf8());
     QObject::connect(reply, &QNetworkReply::finished, [reply, manager, this]() {
         if (reply->error() == QNetworkReply::NoError) {
             QString response = QString::fromUtf8(reply->readAll());
+            //read the response from the json file and convert it to QString
+            QJsonDocument jsonDoc = QJsonDocument::fromJson(response.toUtf8());
+            //then get the text from the json file clear the " " and \n in both sides
+            response = jsonDoc.object().value("choices").toArray()[0].toObject().value("text").toString().remove(0, 2);
             emit responseReceived(response);
         } else {
-            // TODO: error handling
+            // Handle error
+            QByteArray data = reply->readAll();
+            QString errorString = reply->errorString();
+            QNetworkReply::NetworkError error = reply->error();
+            qWarning() << "Network error occurred:" << errorString;
+            qWarning() << "Error response:" << data;
+            switch (error) {
+                case QNetworkReply::ConnectionRefusedError:
+                    // TODO: add error handling logic
+                    break;
+                case QNetworkReply::HostNotFoundError:
+                    // TODO: add error handling logic
+                    break;
+                case QNetworkReply::TimeoutError:
+                    // TODO: add error handling logic
+                    break;
+                default:
+                    // TODO: add error handling logic
+                    break;
+            }
+            emit responseReceived("Error");
         }
         reply->deleteLater();
         manager->deleteLater();
--- test/GPTSessiontest.cpp
+++ test/GPTSessiontest.cpp
@@ -1,14 +1,29 @@
 #include "GPTSession.h"
 #include <QDebug>
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     GPTSession* gptSession= new GPTSession(&app);
-    gptSession->setAPIKey("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDaJJNGHFUvim5e5O6m6xOAHS/RB5A7H5DiAK7957ZqFB1ZbqG37LlVTPEtLHR6rXGoviP2HjLV7SoAkJe9r4Sf2K4pgEPlL0/KKqM7pUsWlsPfhvRurMCNhnMkCEf+oKgQDkcOJualhbxtky9knwTGyRu8FXOG1yP2eVCKiSRr1GOd+PwgnE/Ljy0UGkD1r1TMLf8twmMNcwaE5SI/BLSTsFma/oBkkQyziIzR3tY6PgesAURKH1hSqhLN/lFoy3DKQJ3/LfgHm0s9nlhVTQgQ5hHDfAhN3nU23jEU8naTlVA6xv9TutInPPCQSqJhAhvwDqRkBRCNy91lU7vc9uBmSRdKmJi++XUV4bTu3ou07h8uh0BrVA/iLUSy0nBjJkjv1XgziLt8gfqDP5ULbQFtHDnmT53xEfbPmcIh95OV3cTQb95oynhlMi1puHG5wSYetV6/JzEq6fzM+oTNDMLw2anT2dAhWAEbuyULcExH5c/s1ggPdU6OeWuWrphpNVNTzyhpAYovbGaIjfiLmZQZW3b5gVAUEyL/8fcAkf3pgtUlXxz6QlIFlDiLuFrzu4K52I6y5GKwCGoCJNiHQRKvRt/43gkumOkA6FtkldiRnC5EbY7Aem4qnOGwLDhZWj+AMezkNKsa2kYPOsllEV+9Bbrk0BqF9uvP4mXsxCYlsw==");
-    QObject::connect(gptSession, &GPTSession::responseReceived, [](QString response) {
-        qDebug() << response;
+    gptSession->setAPIKey("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");
+    printf("Connecting...");
+    bool success = QObject::connect(gptSession, &GPTSession::responseReceived, [](QString response) {
+        if (response.isEmpty()) {
+            qWarning() << "Empty response received from the server";
+            // TODO: add error handling logic
+        } else {
+            std::cout<<response.toStdString();
+        }
     });
+    if (!success) {
+        qWarning() << "Failed to connect signals and slots";
+        // TODO: add error handling logic
+    }
+    char str[100];
+    //while(scanf("%s", str) != EOF) {
+        //gptSession->addPrompt(str);
+    //}
     gptSession->addPrompt("Hello,there");
     return app.exec();
 }
