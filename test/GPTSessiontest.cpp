e029eee00c4be4a1435ca061a01abe89d0b7eb81
XChy <xxs_chy@outlook.com>
2023-05-06 12:47:41 +0800

    Simplify GPT test

--- test/GPTSessiontest.cpp
+++ test/GPTSessiontest.cpp
@@ -3,27 +3,23 @@
 #include <QApplication>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     QApplication app(argc, argv);
-    GPTSession* gptSession= new GPTSession(&app);
+    GPTSession* gptSession = new GPTSession(&app);
     gptSession->setAPIKey("");
     printf("Connecting...");
-    bool success = QObject::connect(gptSession, &GPTSession::responseReceived, [](QString response) {
-        if (response.isEmpty()) {
-            qWarning() << "Empty response received from the server";
-            // TODO: add error handling logic
-        } else {
-            std::cout<<response.toStdString();
-        }
-    });
-    if (!success) {
-        qWarning() << "Failed to connect signals and slots";
-        // TODO: add error handling logic
-    }
-    char str[100];
-    //while(scanf("%s", str) != EOF) {
-        //gptSession->addPrompt(str);
-    //}
+
+    QObject::connect(
+        gptSession, &GPTSession::responseReceived, [](QString response) {
+            if (response.isEmpty()) {
+                qWarning() << "Empty response received from the server";
+                // TODO: add error handling logic
+            } else {
+                std::cout << response.toStdString();
+            }
+        });
+
     gptSession->addPrompt("Hello,there");
     return app.exec();
 }
