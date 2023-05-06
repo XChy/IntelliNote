913a17e8b8cbe038d1a04602b68a8401547d3944
Scotchingice <1439921564@qq.com>
2023-05-06 13:21:22 +0800

    gpt_basics_test of prompt_sending

--- test/GPTSessiontest.cpp
+++ test/GPTSessiontest.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     GPTSession* gptSession= new GPTSession(&app);
-    gptSession->setAPIKey("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");
+    gptSession->setAPIKey("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");
     printf("Connecting...");
     bool success = QObject::connect(gptSession, &GPTSession::responseReceived, [](QString response) {
         if (response.isEmpty()) {
