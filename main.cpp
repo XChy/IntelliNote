8eb3bcc3ab18937a2307af130e9c1fa595d124ab
Scotchingice <1439921564@qq.com>
2023-05-22 22:39:13 +0800

    style

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QFile qss("StyleSheet.qss");
-    qss.open(QFile::ReadOnly);
-    a.setStyleSheet(qss.readAll());
-    qss.close();
+    QFile f(":qdarkstyle/dark/darkstyle.qss");
+    QFile f2(":qlightstyle/light/lightstyle.qss");
+    if (!f.exists())   {
+        printf("Unable to set stylesheet, file not found\n");
+    }
+    else   {
+        f.open(QFile::ReadOnly | QFile::Text);
+        QTextStream ts(&f);
+        a.setStyleSheet(ts.readAll());
+    }
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
